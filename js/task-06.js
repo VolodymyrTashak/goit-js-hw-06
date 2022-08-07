415ec826ee84027a337ca1f13b826035a484fb17
VolodymyrTashak <vovavozny33@gmail.com>
2022-08-07 20:56:34 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -4,8 +4,6 @@ inputEl.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
   if (event.currentTarget.value.length === Number(inputEl.dataset.length)) {
-    return (
-      inputEl.classList.add("valid") && inputEl.classList.remove("invalid")
-    );
-  } else inputEl.classList.add("invalid") && inputEl.classList.remove("valid");
+    return inputEl.classList.add("valid"), inputEl.classList.remove("invalid");
+  } else inputEl.classList.add("invalid"), inputEl.classList.remove("valid");
 }
